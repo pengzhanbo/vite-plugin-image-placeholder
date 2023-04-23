955fb18020324c12e7ad8803b21535a9bdc3e15f
pengzhanbo <volodymyr@foxmail.com>
2023-04-24 01:29:29 +0800

    fix: lru-cache import

--- src/cache.ts
+++ src/cache.ts
@@ -1,10 +1,10 @@
-import LRU from 'lru-cache'
+import { LRUCache } from 'lru-cache'
 import type { ImageCacheItem } from './types'
 
-export const bufferCache = new LRU<string, ImageCacheItem>({
+export const bufferCache = new LRUCache<string, ImageCacheItem>({
   max: 500,
 })
 
-export const contentCache = new LRU<string, string>({
+export const contentCache = new LRUCache<string, string>({
   max: 500,
 })
