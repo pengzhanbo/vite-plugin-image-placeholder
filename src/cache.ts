ae040c1fe2595ca414d0bd33b4a4400538b96a90
pengzhanbo <volodymyr@foxmail.com>
2023-05-13 21:30:16 +0800

    perf: replace `quick-lru` to `lru-cache`

--- src/cache.ts
+++ src/cache.ts
@@ -1,10 +1,10 @@
-import QuickLRU from 'quick-lru'
+import { LRUCache } from 'lru-cache'
 import type { ImageCacheItem } from './types'
 
-export const bufferCache = new QuickLRU<string, ImageCacheItem>({
-  maxSize: 500,
+export const bufferCache = new LRUCache<string, ImageCacheItem>({
+  max: 500,
 })
 
-export const contentCache = new QuickLRU<string, string>({
-  maxSize: 500,
+export const contentCache = new LRUCache<string, string>({
+  max: 500,
 })
