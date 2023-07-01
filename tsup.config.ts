b33f235390201d8ab87efd5f75c748e07ad211c2
pengzhanbo <volodymyr@foxmail.com>
2023-07-02 00:57:54 +0800

    build: update deps to latest

---
+++ tsup.config.ts
@@ -0,0 +1,12 @@
+import { defineConfig } from 'tsup'
+
+export default defineConfig({
+  entry: ['src/index.ts'],
+  sourcemap: false,
+  dts: true,
+  splitting: false,
+  clean: true,
+  minify: true,
+  shims: true,
+  format: ['esm', 'cjs'],
+})
